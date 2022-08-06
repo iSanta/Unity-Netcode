486a66c8ab6ea6c9d95c42056773b82ad306d473
Juan Carlos Santa Abreu <jcarlossa120@hotmail.com>
2022-08-06 15:11:29 -0500

    CharacterMovement

--- Assets/Scripts/ThirdPersonMovement.cs
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Netcode;
 
-public class ThirdPersonMovement : MonoBehaviour
+public class ThirdPersonMovement : NetworkBehaviour
 {
 
-    public CharacterController controller;
+    [SerializeField] private CharacterController controller;
     [SerializeField] private float speed = 6f;
     [SerializeField] private float turnSmoothTime = 0.1f;
     [SerializeField] private Transform Cam;
+    [SerializeField] private Camera ownCamera;
+    [SerializeField] private GameObject Cinemachine;
+  
     private float turnSmoothVelocity;
+
+
+
+    private NetworkVariable<Quaternion> rotationNetwork = new NetworkVariable<Quaternion>();
+    private NetworkVariable<Vector3> positionNetwork = new NetworkVariable<Vector3>();
+
+    private void Start()
+    {
+        if (!IsOwner) { return; }
+
+        ownCamera.enabled = true;
+        Cinemachine.SetActive(true);
+        controller.enabled = true;
+    }
 
     private void Update()
     {
+        if (!IsOwner) { return; }
+
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical);
@@ -22,11 +42,31 @@ public class ThirdPersonMovement : MonoBehaviour
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + Cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
 
-            transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
 
-            Vector3 movedir = Quaternion.Euler(0f,targetAngle,0f) * Vector3.forward;
-            controller.Move(movedir.normalized * speed * Time.deltaTime);
+
+            Vector3 movedir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+
+            UpdatePosRotServerRpc(angle, movedir);
+
+            //transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            //controller.Move(movedir.normalized * speed * Time.deltaTime);
+
+            transform.rotation = rotationNetwork.Value;
+            controller.Move(positionNetwork.Value);
         }
+    }
+
+    [ServerRpc]
+    private void UpdatePosRotServerRpc(float rot, Vector3 pos)
+    {
+        UpdatePosRotClientRpc(rot, pos);
+    }
+
+    [ClientRpc]
+    private void UpdatePosRotClientRpc(float rot, Vector3 pos)
+    {
+        rotationNetwork.Value = Quaternion.Euler(0f, rot, 0f); ;
+        positionNetwork.Value = pos.normalized * speed * Time.deltaTime;
     }
 }
